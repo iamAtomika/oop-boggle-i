e79414bcfdbaec2f6c33f242bc55c6a05f22fac2
atomika <erika.holzinger@gmail.com>
2024-06-12 14:13:59 -0500

    init

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,19 @@
-class BoggleBoard:
-  
-  def __init__(self):
-  
+import string
+import random
 
-  def shake(self):
+
+class BoggleBoard:
+
+    def __init__(self):
+        self._letters = string.ascii_uppercase
+        self.board = []
+
+    def shake(self):
+        shake_it = list(self._letters)
+        random.shuffle(shake_it)
+        self.board = shake_it
+        print(shake_it)
+
+
+boggle_board = BoggleBoard()
+boggle_board.shake()
